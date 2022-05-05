61cdaf608e35fb7063a7db10598f39d4d563c756
LeoJuJu05090822 <103226676+LeoJuJu05090822@users.noreply.github.com>
2022-05-04 18:04:52 -0700

    test1

--- Demo.java
+++ Demo.java
@@ -1,1 +1,12 @@
+public class Demo{
+    int age;
+    int height;
+    public Demo(int age, int height){
+        this.age = age;
+        this.height = height;
+    }
 
+    public int average(){
+        return this.height/this.age;
+    }
+}
--- DemoTest.java
+++ DemoTest.java
@@ -1,1 +1,16 @@
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Test;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+
+public class DemoTest{
+    Demo test1 = new Demo(2, 60);
+
+    @Test
+    public void test(){
+        assertEquals(20, test1.average());
+    }
+}
